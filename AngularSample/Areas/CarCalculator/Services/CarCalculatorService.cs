121c47f8f5275584e995cc2b2632440bd54830bd
Janusz Matyjasik <matyjasik.janusz@gmail.com>
2015-09-21 16:30:24 +0200

    qw

---
+++ AngularSample/Areas/CarCalculator/Services/CarCalculatorService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using AngularSample.Areas.CarCalculator.Models;
+using AngularSample.Areas.CarCalculator.ViewModels;
+
+namespace AngularSample.Areas.CarCalculator.Services
+{
+    public class CarCalculatorService
+    {
+        public CarCalculatorSummary CalculateSummary()
+        {
+            return new CarCalculatorSummary(GetFuelEntries(DateTime.MinValue, DateTime.MaxValue));
+        }
+
+        public CarCalculatorSummary CalculateSummary(DateTime startDate, DateTime endDate)
+        {
+            return new CarCalculatorSummary(GetFuelEntries(startDate, endDate));
+        }
+
+        private IList<FuelEntryModel> GetFuelEntries(DateTime startDate, DateTime endDate)
+        {
+            using (FuelEntryDbContext db = new FuelEntryDbContext())
+            {
+                var defaultCar = db.Cars.SingleOrDefault(i => i.IsSelected);
+                if (defaultCar == null)
+                {
+                    throw new InvalidOperationException("Brak domyślnego samochodu");
+                }
+                return db.FuelEntries
+                    .Where(i => i.CarId == defaultCar.Id)
+                    .Where(i => i.Date >= startDate && i.Date <= endDate)
+                    .ToList();
+            }
+        }
+    }
+
+   
+}
---
+++ AngularSample/Areas/CarCalculator/ViewModels/CarCalculatorSummary.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using AngularSample.Areas.CarCalculator.Models;
+
+namespace AngularSample.Areas.CarCalculator.ViewModels
+{
+    public class CarCalculatorSummary
+    {
+        public CarCalculatorSummary(IList<FuelEntryModel> fuelEntries)
+        {
+            MinAmountOf100Km = fuelEntries.Min(i => i.AmountOfFuelPer100Km);
+            MaxAmountOf100Km = fuelEntries.Max(i => i.AmountOfFuelPer100Km);
+
+            MinCost1Km = fuelEntries.Min(i => i.CostOf1Km);
+            MaxCost1Km = fuelEntries.Max(i => i.CostOf1Km);
+
+            SumDistance = fuelEntries.Sum(i => i.Distance);
+            SumAmountOfFuel = fuelEntries.Sum(i => i.AmountOfFuel);
+            SumTotalCost = fuelEntries.Sum(i => i.GasPrice * i.AmountOfFuel);
+        }
+
+        public decimal AmountOfFuelPer100Km { get; set; }
+        public decimal CostOf1Km { get; set; }
+        public decimal DistancePerMonth { get; set; }
+        public decimal MinAmountOf100Km { get; set; }
+        public decimal MaxAmountOf100Km { get; set; }
+        public decimal MinCost1Km { get; set; }
+        public decimal MaxCost1Km { get; set; }
+        public decimal SumDistance { get; set; }
+        public decimal SumAmountOfFuel { get; set; }
+        public decimal SumTotalCost { get; set; }
+
+        public decimal AvgAmountOf100Km
+        {
+            get
+            {
+                Contract.Ensures(SumDistance!=0);
+                return SumAmountOfFuel/SumDistance*100;
+            }
+        }
+
+        public decimal AvgCostOf1Km
+        {
+            get
+            {
+                Contract.Ensures(SumDistance != 0);
+                return SumTotalCost/SumDistance;
+            }
+        }
+    }
+}
