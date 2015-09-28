88219799e9dabc75b68c9915a8fdbf5aa30ea0e4
Janusz Matyjasik <matyjasik.janusz@gmail.com>
2015-09-28 16:57:55 +0200

    new app

---
+++ AngularSample/Areas/ReceiptCollector/ReceiptCollectorAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AngularSample.Areas.ReceiptCollector
+{
+    public class ReceiptCollectorAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ReceiptCollector";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ReceiptCollector_default",
+                "ReceiptCollector/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
